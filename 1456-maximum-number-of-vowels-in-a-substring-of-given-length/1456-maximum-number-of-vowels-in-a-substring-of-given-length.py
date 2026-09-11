4239e9c97635a5ee16b67bb400525971a837acc4
Han Teng <138442301+tengtangtong@users.noreply.github.com>
2026-09-11 22:36:42 +0800

    Time: 47 ms (79.31%), Space: 20 MB (12.45%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,34 +1,30 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
 
-        # change to list
-        s = list(s)
+        # vowels
         vowels = 'AaEeIiOoUu'
 
-        # left and right pointer 
-        left, right = 0, 0
-        vowel_counts = 0
-        max_vowel_counts = 0
+        # start left pointer and right pointer alrady in position k-1
+        left = 0 
+        vowel_counts = sum( 1 for l in s[:k] if l in vowels)
+        max_vowel_counts = vowel_counts
 
-        # right pointer moves first 
-        for right in range(len(s)):
-            
-            # counts vowels as it goes
+        # loop and add
+        for right in range(k, len(s)):
+
             if s[right] in vowels:
                 vowel_counts += 1
-            
-            # when it builds the first substring
-            if right - left + 1 == k:
-                max_vowel_counts = max(max_vowel_counts, vowel_counts)
+            if s[left] in vowels:
+                vowel_counts -=1 
 
-            # if it hits more than the window 
-            while right - left + 1 > k:
-                if s[left] in vowels:
-                    vowel_counts -= 1
-                left += 1
-                max_vowel_counts = max(max_vowel_counts, vowel_counts)
-        
+            max_vowel_counts = max(max_vowel_counts, vowel_counts)
+
+            left += 1
+
         return max_vowel_counts
+
+        
+        
 
 
 
