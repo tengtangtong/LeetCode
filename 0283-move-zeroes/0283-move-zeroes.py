087f610f8ad6e6b56a7a573fc7f3e703010e1bba
Han Teng <138442301+tengtangtong@users.noreply.github.com>
2026-09-11 15:46:13 +0800

    Time: 3 ms (81.74%), Space: 20.5 MB (62.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,6 +1,11 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
+        behind = 0
+        for ahead in range(len(nums)):
+            if nums[ahead] != 0:
+                nums[behind], nums[ahead] = nums[ahead], nums[behind]
+                behind += 1
+
+
+            
+            
